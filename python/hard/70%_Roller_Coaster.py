c85225699edf89d938678da4acd244b15b8d78a7
Irfaan Domun <irfaan@hotmail.fr>
2016-02-14 03:31:47 +0100

    Update and rename 50%_Roller_Coaster.py to 70%_Roller_Coaster.py

--- python/hard/70%_Roller_Coaster.py
+++ python/hard/70%_Roller_Coaster.py
@@ -11,25 +11,32 @@ list_people = []
 for i in xrange(n):
     pi = int(raw_input())
     list_people.append(pi)
-print >> sys.stderr,list_people,l,c,n
 """""
 Compute for each day the  number of people who ride it with the constraint
 """
 res = 0
 previous_sum = 0
 for k in range(c):
+    res_temp = res
     print >> sys.stderr,'...................'
-    for i in range(len(list_people)):
+    for i in range(len(list_people)+1):
         sum_temp = sum(list_people[:i])
         print >> sys.stderr, list_people, list_people[i:], list_people[:i],i,sum_temp,res,l
         
         if sum_temp > l:
-            res = previous_sum
+            res_temp += previous_sum
 
             list_people = list_people[i-1:] + list_people[:i-1]
             break
         else:
             previous_sum = sum_temp
+    """ 
+    If the total number of people in the list is not enough to fill the capacity
+    """
+    if res == res_temp:
+        res += sum(list_people)
+    else:
+        res = res_temp
     print >> sys.stderr, res
 
 # Write an action using print
