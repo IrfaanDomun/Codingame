a6e500af0a1ed487e0381f5128a61bbbdf385a29
Irfaan Domun <irfaan@hotmail.fr>
2016-02-14 03:38:53 +0100

    Update 70%_Roller_Coaster.py

--- python/hard/70%_Roller_Coaster.py
+++ python/hard/70%_Roller_Coaster.py
@@ -1,8 +1,5 @@
 import sys
 import math
-
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
 """""
 Get the data
 """
@@ -16,30 +13,27 @@ Compute for each day the  number of people who ride it with the constraint
 """
 res = 0
 previous_sum = 0
-for k in range(c):
-    res_temp = res
-    print >> sys.stderr,'...................'
-    for i in range(len(list_people)+1):
-        sum_temp = sum(list_people[:i])
-        print >> sys.stderr, list_people, list_people[i:], list_people[:i],i,sum_temp,res,l
-        
-        if sum_temp > l:
-            res_temp += previous_sum
-
-            list_people = list_people[i-1:] + list_people[:i-1]
-            break
-        else:
-            previous_sum = sum_temp
-    """ 
-    If the total number of people in the list is not enough to fill the capacity
-    """
-    if res == res_temp:
-        res += sum(list_people)
+sum_people = sum(list_people)
+for k in range(c):    
+    
+    if sum_people < l:
+        """ 
+        If the total number of people in the list is not enough to fill the capacity
+        """        
+        res += sum_people
     else:
-        res = res_temp
-    print >> sys.stderr, res
-
-# Write an action using print
-# To debug: print >> sys.stderr, "Debug messages..."
-
+        """""
+        If they are more people than the roller coaster's capacity
+        """
+        for i in range(len(list_people)+1):
+            sum_temp = sum(list_people[:i])
+            print >> sys.stderr, list_people, list_people[i:], list_people[:i],i,sum_temp,res,l
+            
+            if sum_temp > l:
+                res += previous_sum
+    
+                list_people = list_people[i-1:] + list_people[:i-1]
+                break
+            else:
+                previous_sum = sum_temp
 print res
